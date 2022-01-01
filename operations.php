9f829e963d7657e98e285c62142e5c6bec9906ad
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-01-01 20:49:06 -0300

    calculating age and basic math operations

---
+++ operations.php
@@ -0,0 +1,21 @@
+<?php
+
+// calculate how old I will be in 10 years:
+$age = 28;
+$ageIn10Years = $age + 10;
+// echo $ageIn10Years;
+
+// calculate how old I was 5 years ago:
+$age = 28;
+$age5YearsAgo = $age - 5;
+// echo $age5YearsAgo;
+
+
+// basic math operations
+$sum = 1 + 1;
+$subtraction = 1 - 1;
+$multiplication = 2 * 2;
+$division = 4 / 2;
+$twoSquared = 2 ** 2; // (exponentiation | squared)
+$twoCubed = 2 ** 3; // (exponentiation | cubed)
+$restDivision = 10 % 3; // (module or remainder of division)
