f9a8a7ac54b307b2d49c01efcec1f18f5b5905e6
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-01-02 14:07:38 -0300

    decisions with "if"

---
+++ decisions.php
@@ -0,0 +1,10 @@
+<?php
+
+$age = 28;
+
+echo "You can only enter if you are over 18 years old." . PHP_EOL;
+
+if ($age >= 18) {
+    echo "You are $age years old." . PHP_EOL;
+    echo 'You may come in.';
+};
