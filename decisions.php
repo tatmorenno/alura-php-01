4e0572a58ce142b99b8c00a02cf60f747f552299
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-01-03 02:12:37 -0300

    elseif

--- decisions.php
+++ decisions.php
@@ -1,6 +1,6 @@
 <?php
 
-$age = 17;
+$age = 18;
 $numberOfPeople = 2;
 
 echo "You can only enter if you are over 18 years old or ";
@@ -9,7 +9,7 @@ echo "from 16 years old accompanied." . PHP_EOL . PHP_EOL;
 if ($age >= 18) {
     echo "You are $age years old. You can come in alone." . PHP_EOL;
     echo 'You may come in.';
-} else if ($age >= 16 && $numberOfPeople > 1) {
+} elseif ($age >= 16 && $numberOfPeople > 1) {
     echo "You are $age years old. You are accompanied and you can come in.";
 } else {
     echo "You're only $age years old and you're not accompanied. So, you can't come in.";
