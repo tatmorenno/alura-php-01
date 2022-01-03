923f01bf640b7a5a1cdaaccd0aa6e58908b131ec
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-01-03 03:04:02 -0300

    repetition with "for"

---
+++ repetition-2.php
@@ -0,0 +1,12 @@
+<?php
+
+for  ($counter = 1; $counter <= 15; $counter++) {
+    echo  "#$counter" . PHP_EOL;
+}
+
+/* different ways to increment:
+ *
+ * $counter = $counter + 1;
+ * $counter += $counter;
+ * $counter++;
+*/
