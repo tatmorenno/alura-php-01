e0dc5597a9a268ed817f81f36134bd6f3156a38f
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2022-01-13 08:27:59 -0300

    Adding an item to the array

--- advancing/foreach.php
+++ advancing/foreach.php
@@ -1,22 +1,27 @@
 <?php
 
 $checkingAccount = [
-    123456789 => [
+    '123.456.789-01' => [
         'holder' => 'Tatiana',
         'balance' => 3000000
     ],
-    123456780 => [
+    '123.456.780-02' => [
         'holder' => 'Joao Neto',
         'balance' => 2000000
     ],
-    123456781 => [
+    '123.456.781-03' => [
     'holder' => 'Cecilia',
     'balance' => 1000000
     ]
 ];
 
+$checkingAccount ['123.456.789-04'] = [
+    'holder' => 'Adrian',
+    'balance' => 50000
+];
+
 foreach ($checkingAccount as $index => $account) {
-    echo $account['holder'] . PHP_EOL;
+    echo $index . " " . $account['holder'] . PHP_EOL;
 
     # display the index
     //echo $index . PHP_EOL;
